5a3ea7776359edf2f01f5552db67cddf2e0489f2
tranvobaoduy <tranduy7777@gmail.com>
2019-03-12 07:40:24 +0700

    bo sung ngay

---
+++ abc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class abc : Form
+    {
+        public abc()
+        {
+            InitializeComponent();
+        }
+
+        private void abc_Load(object sender, EventArgs e)
+        {
+            // an banh cuon
+        } // tran vo bao duy
+    }
+}
